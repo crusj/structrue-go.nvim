22ecda59b52b8da3ede333ac3d813e9b3b215b59
jianglong <jianglong199212@gmail.com>
2022-04-26 16:44:07 +0800

    add tags symbol parse

---
+++ symbol_test.lua
@@ -0,0 +1,13 @@
+require("split")
+local s = require('symbol')
+
+local tags = [[
+AddBufferHighlight	api.go	2643;"	m	access:public	ctype:Nvim	line:2643	signature:(buffer Buffer, srcID int, hlGroup string, line int, startCol int, endCol int)	type:int, error
+]]
+
+for _, each in ipairs(tags:split("\n")) do
+	local cut = s:New(each)
+	for k, v in pairs(cut) do
+		print(k, v)
+	end
+end
