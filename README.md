7cbb6e56bb92e6b992568bdcc227f14104ebb26d
jianglong <jianglong199212@gmail.com>
2022-04-28 00:54:00 +0800

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # structrue-go.nvim
 A better structured display of golang symbol information
+
+The project is not yet completed, the description will be updated when it is completed
--- split.lua
+++
@@ -1,9 +0,0 @@
--- split string by sep
-function string:split(sep)
-	local cuts = {}
-	for v in string.gmatch(self, "[^'" .. sep .. "']+") do
-		table.insert(cuts,v)
-	end
-
-	return cuts
-end
